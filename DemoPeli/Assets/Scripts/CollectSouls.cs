a166d2ba1684490cb0797dc8d9e529c6796ab380
RasmusRimmi <rasmus136@gmail.com>
2023-02-25 13:49:23 +0200

    Rotation

--- DemoPeli/Assets/Scripts/CollectSouls.cs
+++ DemoPeli/Assets/Scripts/CollectSouls.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class CollectSouls : MonoBehaviour
 {
@@ -11,12 +10,13 @@ public class CollectSouls : MonoBehaviour
     void Start()
     {
         souls = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
