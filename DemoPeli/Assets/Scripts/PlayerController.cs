fddf1d9075c26582c20695b5a72c989d5fe3667c
RasmusRimmi <rasmus136@gmail.com>
2023-03-12 15:39:30 +0200

    Player animations

--- DemoPeli/Assets/Scripts/PlayerController.cs
+++ DemoPeli/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public float jumpForce = 10;
     public bool isGrounded;
+    public Animator animator;
+    public Vector2 direction;
+    public bool facingRight;
 
     // Start is called before the first frame update
     void Start()
@@ -19,17 +22,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         hInput = Input.GetAxis("Horizontal");
+        animator.SetFloat("horizontal", Mathf.Abs(Input.GetAxis("Horizontal")));
+        //animator.SetFloat("vertical", Mathf.Abs(Input.GetAxis("Vertical")));
 
-        transform.Translate(Vector2.right * speed * hInput * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
+            animator.SetBool("IsJumping", true);
         }
+
         
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * speed * hInput * Time.deltaTime);
+
+        if(hInput > 0 && facingRight)
+        {
+            Flip();
+        }
+
+        else if (hInput < 0 && !facingRight)
+        {
+            Flip();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -37,6 +58,16 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            animator.SetBool("IsJumping", false);
         }
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 }
