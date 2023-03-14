604e269cac520dfef662db70733b1bc162266f6b
RasmusRimmi <rasmus136@gmail.com>
2023-03-14 18:43:57 +0200

    Souls UI

--- DemoPeli/Assets/Scripts/CollectSouls.cs
+++ DemoPeli/Assets/Scripts/CollectSouls.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class CollectSouls : MonoBehaviour
 {
     public float souls;
+    public TextMeshProUGUI numberOfSouls;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +28,8 @@ public class CollectSouls : MonoBehaviour
         {
             souls = souls + 1;
             Destroy(other.gameObject);
+
+            numberOfSouls.text = souls.ToString();
         }
     }
 }
