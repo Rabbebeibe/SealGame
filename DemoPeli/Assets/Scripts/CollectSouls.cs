2b954ee3ec74e900d6762b043145ebfdd3767825
RasmusRimmi <rasmus136@gmail.com>
2023-03-14 10:23:52 +0200

    Soul

---
+++ DemoPeli/Assets/Scripts/CollectSouls.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectSouls : MonoBehaviour
+{
+    public float souls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        souls = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Soul"))
+        {
+            souls = souls + 1;
+            Destroy(other.gameObject);
+        }
+    }
+}
